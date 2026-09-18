2b815c433f9132c68f7d341591ab82635a9891c0
npinguin <nicky.wessels@gmail.com>
2026-09-18 09:50:17 -0700

    Align Mobility UX runtime version

--- src/entry/00-header-and-navigation.js
+++ src/entry/00-header-and-navigation.js
@@ -40,7 +40,7 @@ Internal structure:
 - HomeBrainChargerAdapter: charger contract mapping
 */
 
-const UX_VERSION = "1.0.0-rc.1";
+const UX_VERSION = "1.0.0-rc.2";
 
 const HB_MOBILITY_BASE_PATH = "/mobility-supervisor";
 const HB_MOBILITY_TABS = [
