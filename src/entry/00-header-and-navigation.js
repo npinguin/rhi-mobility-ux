09755f5c8f22bc1fbb6085547c37ca2dea721767
npinguin <nicky.wessels@gmail.com>
2026-09-20 07:16:28 -0700

    chore: advance Mobility UX closure candidate to rc.4

--- src/entry/00-header-and-navigation.js
+++ src/entry/00-header-and-navigation.js
@@ -40,7 +40,7 @@ Internal structure:
 - HomeBrainChargerAdapter: charger contract mapping
 */
 
-const UX_VERSION = "1.0.0-rc.2";
+const UX_VERSION = "1.0.0-rc.4";
 
 const HB_MOBILITY_BASE_PATH = "/mobility-supervisor";
 const HB_MOBILITY_TABS = [
